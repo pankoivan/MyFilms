462291b4c3e46c9b75483e4187c9570ff3e52e3e
pankoivan <panivan98@yandex.ru>
2023-08-05 18:31:48 +0300

    Экспериментирую с выводом списка фильмов на HTML-страницу

--- src/main/java/org/myfilms/controllers/FilmsController.java
+++ src/main/java/org/myfilms/controllers/FilmsController.java
@@ -3,6 +3,7 @@ package org.myfilms.controllers;
 import org.myfilms.dao.interfaces.FilmDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,11 +15,16 @@ public class FilmsController {
     private FilmDao filmDaoImpl;
 
     @GetMapping("/all-my-films")
-    public String returnIndex() {
-        //System.out.println(filmDaoImpl.findById("328"));
-        filmDaoImpl.findAll()
+    public String returnIndex(Model model) {
+        System.out.println(filmDaoImpl.findById("474"));
+        /*filmDaoImpl.findAll()
                 .getItems()
-                .forEach(System.out::println);
+                .forEach(System.out::println);*/
+
+        model.addAttribute("films", filmDaoImpl.findAll().getItems()
+                .stream()
+                .map(film -> filmDaoImpl.findById(String.valueOf(film.getKinopoiskId()))));
+
         return "films-template";
     }
 
--- src/main/java/org/myfilms/entities/Film.java
+++ src/main/java/org/myfilms/entities/Film.java
@@ -1,5 +1,6 @@
 package org.myfilms.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
